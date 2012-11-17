1d4d437a67827415bad83e21066cd14d71b74d27
Sam Soffes <sam@soff.es>
2012-11-17 13:41:00 -0800

    Preferred host

--- sam_and_ellen.rb
+++ sam_and_ellen.rb
@@ -1,8 +1,17 @@
 class SamAndEllen < Sinatra::Base
+  PREFERRED_HOST = 'samandellen.com'.freeze
+  WEDDING_DATE = Date.parse('2013-09-21').freeze
+
+  if ENV['RACK_ENV'] == 'production'
+    before do
+      if request.host != PREFERRED_HOST
+        redirect to("http://#{PREFERRED_HOST + request.path_info}")
+      end
+    end
+  end
+
   get '/' do
-    today = Date.today
-    wedding = Date.parse('2013-09-21')
-    @days = (wedding - today).to_i
+    @days = (WEDDING_DATE - Date.today).to_i
     erb :index
   end
 end
